ba9242db04b81770aef869d6642d9d0663275119
Liam McGovern <liammcgovern@gmail.com>
2014-01-28 11:29:30 -0500

    Nothing new

--- Assignment1/src/assignment1/Actor.java
+++ Assignment1/src/assignment1/Actor.java
@@ -1,6 +1,24 @@
 package assignment1;
 
+
 public class Actor {
-	actortest;
+	public final static double MIN_SPEED = 0.0;
+	public final static double MAX_SPEED = 150.0;
+
+	private String name;
+	private double strength;
+	private double speed;
+	private double health;
+
+	// Actor Constructor
+	Actor() {	
+		name = "Actor";
+		strength = 0;
+		speed = 0;
+		health = 0;
+
+	}
+	
+	
 
 }
