f60c95ab19dc894e98cf4a9a81ba13a239ad5d5e
Liam McGovern <liammcgovern@gmail.com>
2014-01-28 12:21:46 -0500

    Added Serieal number

--- Assignment1/src/assignment1/Actor.java
+++ Assignment1/src/assignment1/Actor.java
@@ -4,15 +4,18 @@ package assignment1;
 public class Actor {
 	public final static double MIN_SPEED = 0.0;
 	public final static double MAX_SPEED = 150.0;
-
+	
+	private static int actorSerialNumber = 1;
+	
 	private String name;
 	private double strength;
 	private double speed;
 	private double health;
 
+	
 	// Actor Constructor
 	Actor() {	
-		name = "Actor";
+		name = ("Actor" + actorSerialNumber);
 		strength = 0;
 		speed = 0;
 		health = 0;
