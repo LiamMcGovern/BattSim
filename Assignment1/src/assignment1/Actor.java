8913753691814e2df97e071c172b7db9053c3ad0
Liam McGovern <liammcgovern@gmail.com>
2014-01-28 12:52:58 -0500

    updatye

--- Assignment1/src/assignment1/Actor.java
+++ Assignment1/src/assignment1/Actor.java
@@ -1,27 +1,23 @@
 package assignment1;
 
-
 public class Actor {
 	public final static double MIN_SPEED = 0.0;
 	public final static double MAX_SPEED = 150.0;
-	
-	private static int actorSerialNumber = 1;
-	
+
+	private static int actorSerialNumber = 0;
+
 	private String name;
 	private double strength;
 	private double speed;
 	private double health;
 
-	
 	// Actor Constructor
-	Actor() {	
-		name = ("Actor" + actorSerialNumber);
+	Actor() {
+		name = ("Actor" + (actorSerialNumber++));
 		strength = 0;
 		speed = 0;
 		health = 0;
 
 	}
-	
-	
 
 }
